8da34caefae641cf4fd017205691132f51876df1
Jacob Valenta <jacob.valenta@gmail.com>
2013-08-09 11:16:02 -0400

    Creates a Home Page

--- puzzels/views.py
+++ puzzels/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.views.generic import TemplateView
+from django.contrib.auth.forms import AuthenticationForm
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    # def get_context_data(self, **kwargs):
+    #     '''Adds context for the authentication form for inline login'''
+    #     context = super(HomeView, self).get_context_data(**kwargs)
+    #     context.update({
+    #         'login_form': AuthenticationForm
+    #     })
+    #     return context
--- urls.py
+++ urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from sudoku.puzzels.views import HomeView
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sudoku.views.home', name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     # url(r'^sudoku/', include('sudoku.foo.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
