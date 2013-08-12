c39d307a284b02b7e3107cf956b5176a972f2f73
Jacob Valenta <jacob.valenta@gmail.com>
2013-08-12 16:19:45 -0400

    Adds Auth Urls

--- urls.py
+++ urls.py
@@ -9,5 +9,15 @@ urlpatterns = patterns('',
     url(r'^$', HomeView.as_view(), name='home'),
     # url(r'^sudoku/', include('sudoku.foo.urls')),
 
+    # Auth
+	url(r'^', include(patterns('',
+        url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': reverse_lazy('home')}, name="logout"),
+        url(r'^', include('django.contrib.auth.urls')),
+        url(r'^register/$', RegistrationView.as_view(), name="register"),
+    ), namespace='auth')),
+
+	#registration
+    # url(r'^register/$', RegistrationView.as_view(), name="register"),
+
     url(r'^admin/', include(admin.site.urls)),
 )
