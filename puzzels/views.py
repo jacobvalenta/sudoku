6270e4bb3722d71112f6217bea55bee00dca2f51
Jacob Valenta <jacob.valenta@gmail.com>
2013-08-12 16:21:44 -0400

    Adds A puzzel list view

--- puzzels/views.py
+++ puzzels/views.py
@@ -1,13 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, UpdateView, DetailView
 from django.contrib.auth.forms import AuthenticationForm
 
 class HomeView(TemplateView):
     template_name = 'home.html'
 
-    # def get_context_data(self, **kwargs):
-    #     '''Adds context for the authentication form for inline login'''
-    #     context = super(HomeView, self).get_context_data(**kwargs)
-    #     context.update({
-    #         'login_form': AuthenticationForm
-    #     })
-    #     return context
+class PuzzelListView(ListView):
+    template_name = "puzzel_list.html"
+    
+    def get_queryset(self, *args, **kwargs):
+        return Puzzel.objects.all()
+        
